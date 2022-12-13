751ad874963486ec7341a1feff0e72fb87b4407f
Irina <bezdnaya@mail.ru>
2022-12-13 17:56:19 +0200

    47 Заполнили массив случайными числами

---
+++ Task47/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Задайте размер двумерного массива m, n:");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array [i,j] = new Random().Next(1,10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine("");
+}
